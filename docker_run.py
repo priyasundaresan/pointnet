e6865bc0d4413899cd963a29b5b31c70c867fb1e
Priya Sundaresan <priya.sundaresan@berkeley.edu>
2020-05-20 11:55:47 -0700

    working

--- docker_run.py
+++ docker_run.py
@@ -3,5 +3,6 @@ import os
 
 if __name__=="__main__":
     #cmd = "nvidia-docker run -it -p 8888:8888 -p 6006:6006 -v %s:/host priya-mrcnn" % (os.getcwd())
-    cmd = "nvidia-docker run -it -p 6006:6006 -v %s:/host priya-pointnet" % (os.getcwd())
+    #cmd = "nvidia-docker run -it -p 6006:6006 -v %s:/host priya-pointnet" % (os.getcwd())
+    cmd = "nvidia-docker run -it -p 6006:8080 -v %s:/host priya-pointnet" % (os.getcwd())
     code = os.system(cmd)
